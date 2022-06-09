704c5a9ef9da6a55e082aeb6cd19d4ff5b252d33
Deep Debnath <dj.debnath2000@gmail.com>
2022-06-09 23:50:04 +0530

    updated html,css,js files

--- contentScript.js
+++ contentScript.js
@@ -2,7 +2,7 @@
     let youtubeLeftContrils,youtbePlayer;
     let currentVideo="";
 
-    chrome.runtime.onMessage.addLister((obj,sender,response)=>{
+    chrome.runtime.onMessage.addListener((obj,sender,response)=>{
         const {type,value,videoId}=obj;
 
         if(type==="NEW"){
@@ -10,4 +10,16 @@
             newVideoLoaded();
         }
     });
+
+    const newVideoLoaded=()=>{
+        const bookmarkBtnExists=document.getElementsByClassName("bookmark-btn")[0];
+
+        if(!bookmarkBtnExists){
+            const bookmarkBtn=document.createElement("img");
+
+            bookmarkBtn.src=chrome.runtime.getURL("assets/bookmark.png");
+            bookmarkBtn.className="ytp-button"+"bookmark-btn";
+            bookmarkBtn.title="Click to bookmark current timestamp";
+        }
+    }
 })();
