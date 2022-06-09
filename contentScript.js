672c8ca946b4b31ca029640d22b6879d59742f7f
Deep Debnath <dj.debnath2000@gmail.com>
2022-06-09 21:46:57 +0530

    worked on manifest file

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,13 @@
 (()=>{
-    
-})
+    let youtubeLeftContrils,youtbePlayer;
+    let currentVideo="";
+
+    chrome.runtime.onMessage.addLister((obj,sender,response)=>{
+        const {type,value,videoId}=obj;
+
+        if(type==="NEW"){
+            currentVideo=videoId;
+            newVideoLoaded();
+        }
+    });
+})();
